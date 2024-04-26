10194ad359e01f8ec31d735a941425c525333582
DANTE SIEGEL <47492509@ort>
2024-04-26 10:01:08 -0300

    postman mejorado

--- src/controllers/province-controller.js
+++ src/controllers/province-controller.js
@@ -1,9 +1,24 @@
-
-
 import express from 'express';
+import provinces from '../entities/province.js';
 
 const ProvinceRouter = express.Router();
 
+// Endpoint para obtener todas las provincias
+ProvinceRouter.get("/", (req, res) => {
+  res.status(200).json(provinces);
+});
 
+// Endpoint para obtener una provincia por su id
+ProvinceRouter.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const province = provinces.find(province => province.id === id);
+  if (province) {
+    res.status(200).json(province);
+  } else {
+    res.status(404).send("Provincia no encontrada");
+  }
+});
+
+// Otros endpoints para insertar, actualizar y eliminar provincias
 
 export default ProvinceRouter;
---
+++ src/entities/province.js
@@ -0,0 +1,45 @@
+const provinces = [
+    {
+      id: 1,
+      name: "Buenos Aires",
+      full_name: "Provincia de Buenos Aires",
+      latitude: -34.605105,
+      longitude: -58.381555,
+      display_order: 1
+    },
+    {
+      id: 2,
+      name: "Córdoba",
+      full_name: "Provincia de Córdoba",
+      latitude: -31.420083,
+      longitude: -64.188774,
+      display_order: 2
+    },
+    {
+      id: 3,
+      name: "Santa Fe",
+      full_name: "Provincia de Santa Fe",
+      latitude: -31.610657,
+      longitude: -60.697293,
+      display_order: 3
+    },
+    {
+      id: 4,
+      name: "Mendoza",
+      full_name: "Provincia de Mendoza",
+      latitude: -32.889458,
+      longitude: -68.845838,
+      display_order: 4
+    },
+    {
+      id: 5,
+      name: "Salta",
+      full_name: "Provincia de Salta",
+      latitude: -24.782127,
+      longitude: -65.423198,
+      display_order: 5
+    }
+  ];
+  
+  export default provinces;
+  
